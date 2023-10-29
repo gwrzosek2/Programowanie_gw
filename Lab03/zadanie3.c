7dce0241d2c9f6df930e21eec48caa54c4adc8f6
Grzegorz Wrzosek <g.wrzosek.059@studms.ug.edu.pl>
2023-10-29 14:33:42 +0100

    Dodanie 5 zadan

---
+++ Lab03/zadanie3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+   int liczba1, liczba2;
+
+   printf("Podaj pierwsza liczbe: ");
+   scanf("%d", &liczba1);
+   printf("Podaj druga liczbe: ");
+   scanf("%d", &liczba2);
+
+   if(liczba1 > liczba2){
+    printf("liczba: %d jest wieksza od liczby: %d", liczba1, liczba2);
+   }
+   else if(liczba1 < liczba2){
+    printf("liczba: %d jest wieksza od liczby: %d", liczba2, liczba1);
+   }
+   else{
+    printf("liczby %d i %d sa jednakowe", liczba1, liczba2);
+   }
+
+   return 0;
+}
---
+++ Lab03/zadanie5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   char ch;
+   printf("Enter a character: ");
+   scanf("%c", &ch);
+
+   if((ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z") || (ch >= 97 && ch <= 122) || (ch >= 65 && ch <= 90)){
+    printf("Znak: %c jest znakiem z alfabetu", ch);
+   }
+   else{
+    printf("Znak %c nie jest znakiem z alfaetu", ch);
+   }
+
+   return 0;
+}
