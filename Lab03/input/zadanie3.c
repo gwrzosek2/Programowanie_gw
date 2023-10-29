7be94e40edd29ee2b89157af4143aa50b111f71d
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2023-10-29 21:49:08 +0100

    zmiana struktury plikow

--- Lab03/input/zadanie3.c
+++ Lab03/input/zadanie3.c
@@ -9,10 +9,10 @@ int main() {
    scanf("%d", &liczba2);
 
    if(liczba1 > liczba2){
-    printf("liczba: %d jest wieksza od liczby: %d", liczba1, liczba2);
+    printf("liczba 1: %d jest wieksza od liczby: %d", liczba1, liczba2);
    }
    else if(liczba1 < liczba2){
-    printf("liczba: %d jest wieksza od liczby: %d", liczba2, liczba1);
+    printf("liczba 2: %d jest wieksza od liczby: %d", liczba2, liczba1);
    }
    else{
     printf("liczby %d i %d sa jednakowe", liczba1, liczba2);
