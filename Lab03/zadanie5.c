ba819dd1af4191233a0a5392fb6f6d93f95440fc
Grzegorz Wrzosek <g.wrzosek.059@studms.ug.edu.pl>
2023-10-29 15:03:57 +0100

    lab03: 6/16

--- Lab03/zadanie5.c
+++ Lab03/zadanie5.c
@@ -2,10 +2,11 @@
 
 int main() {
    char ch;
-   printf("Enter a character: ");
+   
+   printf("Wprowadz znak lub kod: ");
    scanf("%c", &ch);
 
-   if((ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z") || (ch >= 97 && ch <= 122) || (ch >= 65 && ch <= 90)){
+   if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= 97 && ch <= 122) || (ch >= 65 && ch <= 90)){
     printf("Znak: %c jest znakiem z alfabetu", ch);
    }
    else{
---
+++ Lab03/zadanie6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   int rok;
+
+   printf("Wprowadz rok: ");
+   scanf("%d", &rok);
+
+   if(rok % 4 == 0){
+    printf("Rok %d jest przestepny", rok);
+   }
+   else{
+    printf("Rok %d nie jest przestepny", rok);
+   }
+   
+   return 0;
+}
