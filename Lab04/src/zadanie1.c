8a1947a52e15f24ed8956301095830f5d9ad68ec
Grzegorz Wrzosek <g.wrzosek.059@studms.ug.edu.pl>
2023-11-05 15:53:44 +0100

    Lab04: 2/15

---
+++ Lab04/src/zadanie1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sumakwadratow(int n){
+    int temp = 0;
+    for(int i = 1; i <= n; i++){
+        temp += i * i;
+    }
+
+    return temp;
+}
+
+int main() {
+  int n;
+
+  printf("Podaj liczbe: ");
+  scanf("%d", &n);
+
+  printf("Suma kwadratów liczb od 1 do %d (wliczajac 1 i n) wynosi: %d\n", n, sumakwadratow(n));
+  return 0;
+}
