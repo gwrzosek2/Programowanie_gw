520692a9d043803378718382fbc9135e7088c259
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-01-12 22:04:31 +0100

    Lab05: 6/15

---
+++ Lab05/src/zadanie6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int findIntersectionEnd(int a1, int b1, int a2, int b2) {
+  int a3 = (a1 > a2) ? a1 : a2;
+  int b3 = (b1 < b2) ? b1 : b2;
+  if (a3 <= b3) {
+    return b3;
+  } else {
+    return -1;
+  }
+}
+
+int main() {
+  int a1 = 3;
+  int b1 = 9;
+  int a2 = 5;
+  int b2 = 12;
+
+  int result = findIntersectionEnd(a1, b1, a2, b2);
+
+  if (result != -1) {
+    printf("Koniec przekroju istnieje, wynosi: %d\n", result);
+  } else {
+    printf("Przekrój jest zbiorem pustym\n");
+  }
+
+  return 0;
+}
