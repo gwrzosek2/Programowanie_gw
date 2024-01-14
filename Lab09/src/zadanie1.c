c8264e878f60b54f62b626dce9916cddcd703470
Grzegorz Wrzosek <g.wrzosek.059@studms.ug.edu.pl>
2024-01-14 15:50:17 +0100

    Lab09: 3/11

---
+++ Lab09/src/zadanie1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct klient{
+    char imie[100];
+    char nazwisko[100];
+    int nrTelefonu;
+};
+
+int main(){
+    struct klient k1 = {"Bozenka", "Kowalska", 783999222};
+    struct klient k2 = {"Andzej", "Kowalski", 888222333};
+
+    printf("--------------------------------------------\n");
+    printf("Pierwszy klient:\nImie: %s\nNazwisko: %s\nNumer telefonu: %d\n",k1.imie,k1.nazwisko,k1.nrTelefonu);
+    printf("--------------------------------------------\n");
+    printf("Drugi klient:\nImie: %s\nNazwisko: %s\nNumer telefonu: %d\n",k2.imie,k2.nazwisko,k2.nrTelefonu);
+    printf("--------------------------------------------\n");
+    return 0;
+}
---
+++ Lab09/src/zadanie3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct trojkat{
+    int a;
+    int b;
+    int c;
+};
+
+void isValidTriangle(int a, int b, int c) 
+{ 
+    if (a + b <= c || a + c <= b || b + c <= a) 
+        printf("Trojkat jest niepoprawny");
+    else
+        printf("Trojkat jest poprawny");
+} 
+
+int main(){
+    struct trojkat t1 = {30,10,5};
+    struct trojkat t2 = {6,12,7};
+
+    printf("--------------------------------------------\n");
+    printf("Pierwszy Trojkat: (a: %d b: %d c: %d)\n",t1.a, t1.b, t1.c);
+    isValidTriangle(t1.a,t1.b,t1.c);
+    printf("\n--------------------------------------------\n");
+    printf("Drugi Trojkat: (a: %d b: %d c: %d)\n",t2.a, t2.b, t2.c);
+    isValidTriangle(t2.a,t2.b,t2.c);
+    printf("\n--------------------------------------------\n");
+    return 0;
+}
